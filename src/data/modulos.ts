184bfdeb0afb13fb336a8f69b32e2b1f7641c7fd
César Jarith Coello <jarith182@gmail.com>
2024-06-20 21:04:04 -0600

    creado modulos

--- src/data/modulos.ts
+++ src/data/modulos.ts
@@ -1,13 +1,30 @@
 interface Modulo {
     nombre: string,
+    descripcion: string,
     url: string,
     img: string,
 }
 
 export const modulos: Modulo[] = [
-    {nombre: 'Pacientes', url: 'pacientes', img: 'galeria_01.jpg'},
-    {nombre: 'Citas', url: 'citas', img: 'galeria_02.jpg'},
-    {nombre: 'Alertas y Recordatorios', url: 'alertas', img: 'galeria_03.jpg'},
-    {nombre: 'Inventario', url: 'inventario', img: 'galeria_04.jpg'},
+    {
+        nombre: 'Administración de Expedientes',
+        descripcion:'Digitaliza y gestiona los expedientes de tus pacientes de manera segura y organizada. Desde la primera consulta hasta el final del tratamiento, mantén un registro detallado y fácilmente accesible.',
+        url: 'expedientes',
+        img: 'modulo_01.jpg'},
+    {
+        nombre: 'Programación de Citas',
+        descripcion:'Organiza y gestiona las citas médicas de manera eficiente. Evita conflictos de horarios y mejora la experiencia del paciente con recordatorios automáticos y una vista clara de la agenda.',
+        url: 'citas', 
+        img: 'modulo_02.png'},
+    {
+        nombre: 'Alertas y Recordatorios',
+        descripcion:'Configura alertas y recordatorios para seguimientos, revisiones y tareas pendientes, asegurando un cuidado continuo y adecuado del paciente.', 
+        url: 'alertas', 
+        img: 'modulo_03.jpg'},
+    {
+        nombre: 'Manejo de Inventarios',
+        descripcion:'Administra el inventario de medicamentos y suministros médicos de tu clínica. Asegura un control preciso y reduce el riesgo de desabastecimientos y errores.', 
+        url: 'inventario', 
+        img: 'modulo_04.jpg'},
 ]
 
