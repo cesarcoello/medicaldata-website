fdd67f3db02d885634c78e983410592b1e5e8033
César Jarith Coello <jarith182@gmail.com>
2024-06-26 13:42:50 -0600

    Componente CardPlan

--- src/data/planes.ts
+++ src/data/planes.ts
@@ -2,7 +2,7 @@ interface Planes {
     nombre: string,
     descripcion: string,
     precio: string,
-    caracteristicas: string[],
+    caracteristicas: string[]
 }
 
 export const planes: Planes[] = [
@@ -17,7 +17,29 @@ export const planes: Planes[] = [
                     'Manejo de Inventarios (básico)',
                     'Soporte por correo electrónico'
         ]
-
-
+     },
+     {
+        nombre: 'Plan Profesional',
+        descripcion: 'Perfecto para clínicas en crecimiento que requieren más funcionalidades y mejor soporte.',
+        precio: '1000',
+        caracteristicas:[
+                    'Todo en el Plan Básico',
+                    'Exportación de expedientes en formato PDF',
+                    'Recordatorios automáticos a pacientes (SMS y correo electrónico)',
+                    'Reportes de Inventario avanzados',
+                    'Soporte por correo electrónico y chat'
+        ]
+     },
+     {
+        nombre: 'Plan Premium',
+        descripcion: 'La solución más completa para clínicas establecidas que buscan optimizar todas sus operaciones.',
+        precio: '2000',
+        caracteristicas:[
+                    'Todo en el Plan Profesional',
+                    'Recordatorios automáticos a pacientes (SMS, correo electrónico y WhatsApp)',
+                    'Reportes y análisis personalizados',
+                    'Soporte prioritario por teléfono, correo electrónico y chat',
+                    'Acceso a futuras expansiones y módulos adicionales'
+        ]
      }
 ]
